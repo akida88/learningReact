8fd9573dba775317c7fc626a59f9e9f4fe98d94c
akida88 <tinakrysteva@gmail.com>
2020-12-09 18:02:20 +0200

    working on

--- react-complete-app/src/App.js
+++ react-complete-app/src/App.js
@@ -10,14 +10,18 @@ const App = props => {
     ]);
   const [showPersons,setShowPersons] = useState(false);
  // sining a function to a property
-  const showPersonsHandler = () => {
+  const showPersonsHandler = (e) => {
+    e.persist();
+    const current = e.target.style['background-color'];
+    const background = current ==='salmon'? 'green':"red";
+    e.target.style.background = background;
     setShowPersons(!showPersons);
   };
 
- const nameChangedHandler = (event,index) => {
-    debugger;
+ const nameChangedHandler = (event, index) => {
+    // debugger;
     const newPersons = [...persons];
-    newPersons[index].name =  event.target.value;
+    newPersons[index].name = event.target.value;
     setPersons(newPersons);
   };
 
@@ -25,33 +29,94 @@ const App = props => {
     persons.splice(personIndex, 1);
     setPersons([...persons]);
   }
+  
+  // render () {
+    const style = {
+        backgroundColor: 'green',
+        color: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer',
+        hover: {
+          backgroundColor: 'pink',
+          color: 'black',
+        }
+    };
 
-  const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-  };
-  return (
-    <div className="App">
-      <h1>Hi, I'm React App.</h1>
-      <p>This is really working!</p>
-      <button
-      style={style} // adding a style property
-      onClick={showPersonsHandler}>Show persons
-      </button>
+    // style.backgroundColor = 'red';
+    // style[':hover']: = {
+    //   backgroundColor: 'salmon',
+    //   color: 'black'
+    // }
+  
 
-      {showPersons && persons.map((person, index) =>  <Person
-       click={() => deletePersonHandler(index)}
-       name={person.name}
-       age={person.age}
-       key={`${person.name}${index}`}
-     >
-       My Hobbies: Dancing
-       </Person>)}
-    </div>
-  );
-}
+    // (style.backgroundColor = 'red') {
+    //   style[':hover'] = {
+    //     backgroundColor: 'salmon',
+    //     color: 'black'
+    //   };
+    //}
+
+    // if (backgroundColor = 'red') {
+    //   return (hover: 'salmon')
+    // }
+
+    // let classes = ['red', 'bold'].join(' ');
+    const classes = [];
+    if (persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+   
+    const changeBackgroundOnEnter=(e)=> {
+      e.persist();
+      const current = e.target.style['background-color'];
+      const  onHover = current==="green"?"lightgreen":"salmon"
+        e.target.style.background = onHover;
+    }
+
+    const changeBackgroundOnLeave=(e)=> {
+      e.persist();
+      const current = e.target.style['background-color'];
+      const  onHover = (current==="lightgreen"|| current==="green")?"green":"red"
+       //true|false  const x =  ""==="x" ?"x":"y";
+       //== === 
+      //  const y = 2 == "2"; //true
+      //  const x= 2==="2";//false
+      //  const z = Object.is(2,"2");
+      //  console.log(z);
+      //  console.log(y);
+      //  console.log(x);
+       debugger;
+        e.target.style.background = onHover;
+    }
+    return (
+        <div className="App">
+          <h1>Hi, I'm React App.</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+          style={style} // adding a style property
+           onClick={ (e)=>showPersonsHandler(e)}
+           onMouseEnter={changeBackgroundOnEnter}
+           onMouseLeave={changeBackgroundOnLeave}
+           >
+             Show persons
+          </button>
+
+           {showPersons && persons.map((person, index) =>  <Person
+           click={() => deletePersonHandler(index)}
+           name={person.name}
+           age={person.age}
+           key={`${person.name}${index}`}
+           changed={nameChangedHandler}
+          >
+          My Hobbies: Dancing
+          </Person>)}
+        </div>
+    );
+  }
 
 export default App;
--- react-complete-app/src/Person/Person.js
+++ react-complete-app/src/Person/Person.js
@@ -7,17 +7,37 @@
 // using js6 for react and creating a variable with const
 // const = arrow function sintax for react
 import React from 'react';
+import styled from 'styled-components';
 
-import './Person.css';
+//import './Person.css';
+const StyledDiv = styled.div`
+   width: 60%;
+   margin: 16px auto;
+   border: 1px solid #eee;
+   box-shadow: 0 2px 3px #ccc;
+   padding: 16px;
+   text-align: center;
 
+   @media (min-widthL 500px) {
+      width: 450px;
+   }
+`;
+
+// const person = props => {}
 const person = ( {click,name,age,children} ) => {
+   const style = {
+      '@media (min-width:500px)': {
+         width: '450px'
+      }
+   };
    return (
-      <div className="Person">
+     // <div className="Person" style={style}>
+      <StyledDiv>
          <p onClick={click}>I'm {name} and I am {age} years old!</p>
          <p>{children}</p>
          <input type="text"   defaultValue={name} />
-      </div>
-    )
+      </StyledDiv>
+    );
 };
 
 // to call this element we need to import React from the react package/ look the import line
